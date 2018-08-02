36d4c7b8ba0a663832f4756acec5b8eef673cd9a
Jesse B. Hannah <jesse@jbhannah.net>
2018-08-02 16:48:03 -0700

    Core Pokedex class
    Provides a wrapper around the SQLAlchemy database engine and session
    object, with methods to query and wrap sets of database operations in a
    transaction.

---
+++ ttgn/pokedex/__init__.py
@@ -0,0 +1,47 @@
+"""Handles database connection and session management for Pokédex instances."""
+import contextlib
+import os
+
+import sqlalchemy.orm
+import ttgn
+
+
+class Pokedex:
+    """Wrapper for the SQLAlchemy database engine and session object."""
+
+    def __init__(self, uri=None):
+        """Initialize the database if necessary, and create the engine.
+
+        Args:
+            uri (str): Database connection string to use. Defaults to an on-
+                disk SQLite database ``pokedex.sqlite`` in the root directory
+                of the Pokédex project.
+        """
+        if uri is None:
+            path = os.path.realpath(os.path.join(ttgn.__path__[0], '..'))
+            uri = 'sqlite:///{}'.format(os.path.join(path, 'pokedex.sqlite3'))
+
+        engine = sqlalchemy.create_engine(uri)
+        self._session = sqlalchemy.orm.sessionmaker(bind=engine)
+
+    def query(self, *args, **kwargs):
+        """Perform a single query against the database."""
+        with self.session_scope() as session:
+            return session.query(*args, **kwargs)
+
+    @contextlib.contextmanager
+    def session_scope(self):
+        """Provide a transactional scope around a series of database operations.
+
+        This method should only be used in advanced cases where a series of
+        operations need to be treated as a discrete transaction, e.g. running
+        multiple select queries in building a single HTTP response."""
+        session = self._session()
+        try:
+            yield session
+            session.commit()
+        except:
+            session.rollback()
+            raise
+        finally:
+            session.close()
