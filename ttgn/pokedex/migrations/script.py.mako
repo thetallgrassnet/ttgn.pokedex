723c00ef72017fc487c3c51c50a3b4b8b850555c
Jesse B. Hannah <jesse@jbhannah.net>
2018-09-19 13:01:18 -0700

    Data migrations framework

--- ttgn/pokedex/migrations/script.py.mako
+++ ttgn/pokedex/migrations/script.py.mako
@@ -5,8 +5,9 @@ Revises: ${down_revision | comma,n}
 Create Date: ${create_date}
 
 """
-from alembic import op
+from alembic import context, op
 import sqlalchemy as sa
+import ttgn.pokedex.utils
 ${imports if imports else ""}
 
 # revision identifiers, used by Alembic.
@@ -17,8 +18,37 @@ depends_on = ${repr(depends_on)}
 
 
 def upgrade():
-    ${upgrades if upgrades else "pass"}
+    if not ttgn.pokedex.utils.if_x_argument('no-schema', False):
+        schema_upgrade()
+
+    if not ttgn.pokedex.utils.if_x_argument('no-data', False):
+        data_upgrade()
 
 
 def downgrade():
+    if not ttgn.pokedex.utils.if_x_argument('no-data', False):
+        data_downgrade()
+
+    if not ttgn.pokedex.utils.if_x_argument('no-schema', False):
+        schema_downgrade()
+
+
+def schema_upgrade():
+    ${upgrades if upgrades else "pass"}
+
+
+def schema_downgrade():
     ${downgrades if downgrades else "pass"}
+
+
+def data_upgrade():
+    try:
+        ttgn.pokedex.utils.load_data_migration_if_exists(${repr(up_revision)}, 'upgrade')
+    except Exception:
+        data_downgrade()
+        schema_downgrade()
+        raise
+
+
+def data_downgrade():
+    ttgn.pokedex.utils.load_data_migration_if_exists(${repr(up_revision)}, 'downgrade')
--- ttgn/pokedex/utils.py
+++ ttgn/pokedex/utils.py
@@ -2,3 +2,90 @@ def snake_case(name):
     import re
     s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
     return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+
+
+def if_x_argument(arg, default):
+    from alembic import context
+    return context.get_x_argument(as_dictionary=True).get(arg, default)
+
+
+def load_data_migration_if_exists(rev, direction):
+    if not direction in ['upgrade', 'downgrade']:
+        raise RuntimeError('Invalid data migration direction')
+
+    import pathlib
+    import ttgn.pokedex
+
+    data_path = pathlib.Path(ttgn.pokedex.base_path, 'ttgn', 'pokedex',
+                             'migrations', 'data')
+    data_migrations = data_path.glob('{}_{}_*.csv'.format(rev, direction))
+
+    for file in data_migrations:
+        import csv
+        import re
+
+        data = {'insert': [], 'update': [], 'delete': []}
+        model = re.match(r'{}_{}_(.*)\.csv'.format(rev, direction),
+                         file.name).group(1)
+
+        with file.open(newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                if not 'id' in row:
+                    raise KeyError('id')
+
+                operation = row.pop('_operation')
+                if not operation in data:
+                    raise RuntimeError('Invalid data operation')
+
+                data[operation].append(row)
+
+        if data['insert']:
+            from alembic.op import bulk_insert
+
+            table = import_model(model).__table__
+            bulk_insert(table, data['insert'])
+
+        if data['update']:
+            from alembic.op import execute
+            from sqlalchemy.sql.expression import bindparam
+
+            table = import_model(model).__table__
+            stmt = table.update().where(table.c.id == bindparam('id')).values({
+                key: bindparam(key)
+                for key in data['update'][0].keys() if not key == 'id'
+            })
+
+            execute(stmt, data['update'])
+
+        if data['delete']:
+            from alembic.op import execute
+            from sqlalchemy.sql.expression import bindparam
+
+            table = import_model(model).__table__
+            stmt = table.delete().where(table.c.id == bindparam('id'))
+
+            execute(stmt, data['delete'])
+
+
+def import_model(model_path):
+    return import_string('ttgn.pokedex.models.{}'.format(model_path))
+
+
+def import_string(dotted_path):
+    """Import a dotted module path and return the entity designated by the last
+    name in the path."""
+    try:
+        module_path, class_name = dotted_path.rsplit('.', 1)
+    except ValueError:
+        raise ImportError(
+            '{} doesn\'t look like a module path'.format(dotted_path))
+
+    import importlib
+    module = importlib.import_module(module_path)
+
+    try:
+        return getattr(module, class_name)
+    except AttributeError:
+        raise ImportError('Module {} has no class or attribute {}'.format(
+            module_path, class_name))
