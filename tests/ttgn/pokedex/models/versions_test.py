cb340017479e2678567aa367b338ce967663905e
Jesse B. Hannah <jesse@jbhannah.net>
2018-09-19 14:52:18 -0700

    Test Generation.__str__()

--- tests/ttgn/pokedex/models/versions_test.py
+++ tests/ttgn/pokedex/models/versions_test.py
@@ -4,3 +4,9 @@ class TestGeneration(object):
 
         query = pokedex.query(Generation)
         assert len(query.all()) == 7
+
+    def test_str(self, pokedex):
+        from ttgn.pokedex.models.versions import Generation
+
+        generation = pokedex.query(Generation).filter(Generation.id == 1).one()
+        assert str(generation) == 'Generation 1'
