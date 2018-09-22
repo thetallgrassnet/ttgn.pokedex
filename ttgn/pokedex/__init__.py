6a92bddb08ba0f1fb575064a82a3d1762b74652f
Jesse B. Hannah <jesse@jbhannah.net>
2018-09-21 17:05:30 -0700

    Accept engine parameter to Pokedex constructor

--- ttgn/pokedex/__init__.py
+++ ttgn/pokedex/__init__.py
@@ -14,13 +14,15 @@ base_path = os.path.realpath(
 class Pokedex:
     """Wrapper for the SQLAlchemy database engine and session object."""
 
-    def __init__(self, uri=None, migrate=True, debug=False):
+    def __init__(self, uri=None, engine=None, migrate=True, debug=False):
         """Initialize the database if necessary, and create the engine.
 
         Args:
             uri (str): Database connection string to use. Defaults to an on-
                 disk SQLite database ``pokedex.sqlite`` in the root directory
                 of the Pokédex project.
+            engine (sqlalchemy.engine.Engine): Existing SQLAlchemy engine to
+                use instead of the Pokédex instance creating its own.
             migrate (bool): If True, automatically run database migrations on
                 instantiation.
             debug (bool): If True, output detailed SQLAlchemy debug logging.
@@ -28,12 +30,13 @@ class Pokedex:
         self.debug = debug
         self.logger = logging.getLogger(__name__)
 
-        if uri is None:
-            uri = 'sqlite:///{}'.format(
-                os.path.join(base_path, 'pokedex.sqlite3'))
+        if engine is None:
+            if uri is None:
+                uri = 'sqlite:///{}'.format(
+                    os.path.join(base_path, 'pokedex.sqlite3'))
 
-        self.logger.info('Using Pokédex database at {}'.format(uri))
-        engine = sqlalchemy.create_engine(uri, echo=self.debug)
+            self.logger.info('Using Pokédex database at {}'.format(uri))
+            engine = sqlalchemy.create_engine(uri, echo=self.debug)
 
         self._Session = sqlalchemy.orm.sessionmaker(bind=engine)
 
