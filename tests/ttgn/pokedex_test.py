4a21a7e4825bf0f40181b12804394178daefc04d
Jesse B. Hannah <jesse@jbhannah.net>
2018-09-25 11:58:36 -0700

    Finish __init__.py tests

--- tests/ttgn/pokedex_test.py
+++ tests/ttgn/pokedex_test.py
@@ -1,5 +1,27 @@
+import pytest
+
+
 class TestPokedex(object):
-    def test_migrate(self, pokedex):
+    def test_init_engine(self):
+        import os
+
+        from sqlalchemy import create_engine
+        from ttgn.pokedex import Pokedex
+
+        path = os.path.realpath(
+            os.path.join(
+                os.path.dirname(__file__), 'pokedex_test_init_engine.sqlite3'))
+        uri = 'sqlite:///{}'.format(path)
+        engine = create_engine(uri)
+        pokedex = Pokedex(engine)
+
+        try:
+            query = pokedex.query('1')
+            assert query.all() == [(1, )]
+        finally:
+            os.remove(path)
+
+    def test_init_migrate_true(self, pokedex):
         query = pokedex.query('version_num from alembic_version')
         assert query.all() == [('317f62cffc9a', )]
 
@@ -7,3 +29,9 @@ class TestPokedex(object):
         query = pokedex.query('1')
         assert str(query) == 'SELECT 1'
         assert query.all() == [(1, )]
+
+    def test_session_scope_exception(self, pokedex):
+        with pytest.raises(AssertionError):
+            with pokedex.session_scope() as session:
+                query = session.query('1')
+                assert query.all() == [(2, )]
