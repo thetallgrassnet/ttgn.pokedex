a728cadab3274793eaefcea15a32499f544fb679
Jesse B. Hannah <jesse@jbhannah.net>
2018-09-25 16:03:45 -0700

    Evaluate resource_filename only if necessary

--- ttgn/pokedex/__init__.py
+++ ttgn/pokedex/__init__.py
@@ -1,67 +1,56 @@
 """Handles database connection and session management for Pokédex instances."""
 import logging
-import os
 from contextlib import contextmanager
 
 import pkg_resources
-
 from alembic import command, config
 from sqlalchemy import create_engine
 from sqlalchemy.engine import Engine
 from sqlalchemy.orm import sessionmaker
 
-DEFAULT_DATABASE_URI = 'sqlite:///{}'.format(
-    pkg_resources.resource_filename(__name__, 'pokedex.sqlite3'))
-
 
 class Pokedex:
-    """Wrapper for the SQLAlchemy database engine and session object."""
+    """Wrapper for the SQLAlchemy database engine and session object.
+
+    Args:
+        engine_or_uri (str|sqlalchemy.engine.Engine): Existing SQLAlchemy
+            engine or database connection string to use. Defaults to an on-
+            disk SQLite database ``pokedex.sqlite3`` in the root directory
+            of the ``ttgn.pokedex`` package.
+        migrate (bool): If True, automatically run database migrations on
+            instantiation.
+        alembic_cfg (alembic.config.Config): Existing Alembic configuration
+            to use for running migrations. Must include the Pokédex
+            migrations in its ``versions_location``.
+        debug (bool): If True, output detailed SQLAlchemy debug logging.
+            Mostly for development, and only used if an existing engine
+            was not passed in with the ``engine_or_uri`` parameter.
+    """
 
     def __init__(self,
-                 engine_or_uri=DEFAULT_DATABASE_URI,
+                 engine_or_uri=None,
                  migrate=True,
                  alembic_cfg=None,
                  debug=False):
-        """Initialize the database if necessary, and create the engine.
-
-        Args:
-            engine_or_uri (str|sqlalchemy.engine.Engine): Existing SQLAlchemy
-                engine or database connection string to use. Defaults to an on-
-                disk SQLite database ``pokedex.sqlite3`` in the root directory
-                of the ``ttgn.pokedex`` package.
-            migrate (bool): If True, automatically run database migrations on
-                instantiation.
-            alembic_cfg (alembic.config.Config): Existing Alembic configuration
-                to use for running migrations. Must include the Pokédex
-                migrations in its ``versions_location``.
-            debug (bool): If True, output detailed SQLAlchemy debug logging.
-                Mostly for development, and only used if an existing engine
-                was not passed in with the ``engine`` parameter.
-        """
         self.debug = debug
         self.logger = logging.getLogger(__name__)
 
-        if isinstance(engine_or_uri, Engine):
-            engine = engine_or_uri
-        else:
-            self.logger.info(
-                'Using Pokédex database at {}'.format(engine_or_uri))
-            engine = create_engine(engine_or_uri, echo=self.debug)
-
+        engine = self._get_database_engine(engine_or_uri)
         self._Session = sessionmaker(bind=engine)
 
         if migrate:
-            if alembic_cfg is None:
-                alembic_cfg = config.Config(
-                    pkg_resources.resource_filename(__name__, 'alembic.ini'))
-
+            alembic_cfg = config.Config(
+                pkg_resources.resource_filename(
+                    __name__,
+                    'alembic.ini')) if alembic_cfg is None else alembic_cfg
             alembic_cfg.attributes['connectable'] = engine
 
             self.logger.debug('Running migrations')
             command.upgrade(alembic_cfg, 'ttgn.pokedex@head')
 
     def query(self, *args, **kwargs):
-        """Perform a single query against the database."""
+        """Perform a single query against the database. All arguments are passed
+        along to the ``.query()`` method of a SQLAlchemy session."""
         with self.session_scope() as session:
             return session.query(*args, **kwargs)
 
@@ -81,3 +70,14 @@ class Pokedex:
             raise
         finally:
             session.close()
+
+    def _get_database_engine(self, engine_or_uri):
+        if isinstance(engine_or_uri, Engine):
+            return engine_or_uri
+
+        uri = 'sqlite:///{}'.format(
+            pkg_resources.resource_filename(
+                __name__,
+                'pokedex.sqlite3')) if engine_or_uri is None else engine_or_uri
+        self.logger.info('Using Pokédex database at {}'.format(uri))
+        return create_engine(uri, echo=self.debug)
