8ff35eacbe206f18650d5da9aa9f42754dabb658
Jesse B. Hannah <jesse@jbhannah.net>
2018-09-28 15:44:11 -0700

    Create belongs_to decorator

--- ttgn/pokedex/models/base.py
+++ ttgn/pokedex/models/base.py
@@ -1,13 +1,41 @@
 """SQLAlchemy model declarative base configuration."""
 import inflect
-import sqlalchemy
+import sqlalchemy.orm
 from sqlalchemy.ext import declarative
 
 from ttgn.pokedex.utils import snake_case
 
 engine = inflect.engine()
 
-class Base():
+
+def belongs_to(target, name=None, backref=None, nullable=False):
+    """Decorator that creates a foreign key column and relationship on the
+    decorated class pointing to the target class.
+
+    If a name or backref parameter are not provided, the name of the target
+    class and the pluralized name of the decorated class are used to generate
+    the relationship and backref name respectively."""
+
+    def decorator(cls):
+        _name = snake_case(target.__name__) if name is None else name
+        _backref = cls.__pluralname__ if backref is None else backref
+
+        setattr(
+            cls, '{}_id'.format(_name),
+            sqlalchemy.Column(
+                sqlalchemy.Integer,
+                sqlalchemy.ForeignKey(target.id),
+                nullable=nullable))
+
+        setattr(cls, _name,
+                sqlalchemy.orm.relationship(target, backref=_backref))
+
+        return cls
+
+    return decorator
+
+
+class Base:
     """Base class for SQLAlchemy declarative base."""
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
 
--- ttgn/pokedex/models/versions.py
+++ ttgn/pokedex/models/versions.py
@@ -1,7 +1,7 @@
 """Core-series game related models."""
 import sqlalchemy.orm
 
-from .base import Base
+from .base import Base, belongs_to
 
 
 class Generation(Base):
@@ -11,27 +11,18 @@ class Generation(Base):
         return 'Generation {}'.format(self.id)
 
 
+@belongs_to(Generation)
 class VersionGroup(Base):
     """Model representing a group of versions within a core-series
     generation."""
-    generation_id = sqlalchemy.Column(
-        sqlalchemy.Integer,
-        sqlalchemy.ForeignKey(Generation.id),
-        nullable=False)
-
-    generation = sqlalchemy.orm.relationship(
-        Generation, backref='version_groups')
 
     def __str__(self):
         return ''.join([v.acronym for v in self.versions])
 
 
+@belongs_to(VersionGroup)
 class Version(Base):
     """Model representing a core-series game version."""
-    version_group_id = sqlalchemy.Column(
-        sqlalchemy.Integer,
-        sqlalchemy.ForeignKey(VersionGroup.id),
-        nullable=False)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=False, unique=True)
     acronym = sqlalchemy.Column(
         sqlalchemy.String(2), nullable=False, unique=True)
@@ -41,8 +32,6 @@ class Version(Base):
         secondary=VersionGroup.__table__,
         uselist=False,
         backref='versions')
-    version_group = sqlalchemy.orm.relationship(
-        VersionGroup, backref='versions')
 
     def __str__(self):
         return self.name
