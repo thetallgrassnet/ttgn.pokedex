7e99dfe2e591fc91776b5ed487082097865d02ad
Jesse B. Hannah <jesse@jbhannah.net>
2018-09-29 09:33:25 -0700

    Add subtag hybrid_property to Language model

--- tests/ttgn/pokedex/models/multilang_test.py
+++ tests/ttgn/pokedex/models/multilang_test.py
@@ -10,3 +10,29 @@ class TestLanguage:
         """Test that data migrations are run correctly for the model."""
         query = pokedex.query(Language)
         assert query.count() == 17
+
+    def test_str(self, pokedex):
+        """Test that the subtag is returned."""
+        cn = pokedex.query(Language).get(12)
+        assert str(cn) == 'cmn-latn-cn-pinyin'
+
+    class TestSubtag:
+        """Test the ttgn.pokedex.models.multilang.Language.subtag
+        property."""
+
+        def test_subtag_language_only(self, pokedex):
+            """Test that the correct subtag with only the language identifier is
+            returned."""
+            en = pokedex.query(Language).get(5)
+            assert en.subtag == 'en'
+
+        def test_subtag_concatenated(self, pokedex):
+            """Test that the concatenated subtag is returned."""
+            ja = pokedex.query(Language).get(4)
+            assert ja.subtag == 'ja-latn-x-official'
+
+        def test_subtag_query(self, pokedex):
+            """Test querying by subtag."""
+            fr = pokedex.query(Language).filter(
+                Language.subtag == 'fr-fr').one()
+            assert fr.id == 14
--- ttgn/pokedex/models/multilang.py
+++ ttgn/pokedex/models/multilang.py
@@ -1,5 +1,7 @@
 """Translation-related models and utility functions."""
 import sqlalchemy
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.sql import case
 
 from .base import Base
 
@@ -12,3 +14,19 @@ class Language(Base):
     script = sqlalchemy.Column(sqlalchemy.String(8))
     region = sqlalchemy.Column(sqlalchemy.String(2))
     variant = sqlalchemy.Column(sqlalchemy.String(16))
+
+    @hybrid_property
+    def subtag(self):
+        return '-'.join(
+            k for k in [self.language, self.script, self.region, self.variant]
+            if k is not None)
+
+    @subtag.expression
+    def subtag(cls):
+        return cls.language + \
+            case([(cls.script != None, '-' + cls.script)], else_='') + \
+            case([(cls.region != None, '-' + cls.region)], else_='') + \
+            case([(cls.variant != None, '-' + cls.variant)], else_='')
+
+    def __str__(self):
+        return self.subtag
